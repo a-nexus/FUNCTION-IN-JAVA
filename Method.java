a4ad3bc0aab558951f1f8cdd87e47f1e1d57f81c
a-nexus <anuragsingh77077@gmail.com>
2026-02-14 23:29:17 +0530

    METHODS

---
+++ Method.java
@@ -0,0 +1,54 @@
+//SO HERE IS MY FIRST FUNCTION PROGRAM IN JAVA
+
+// PRINT A GIVEN IN A FUNCTION
+
+// import java.util.*;
+// import java.util.*;
+// public class Function{//CLASS NAME 
+//     public static void printMyName(String Name){//HERE IS WE DECLARIED OUR FUNCTION
+//         System.out.print(Name);
+//     }
+//     public static void main(String [] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("ENTER YOUR NAME:");
+//         String your=sc.nextLine();
+//         printMyName(your);//HERE WE CALL OUR FUNCTION
+//     }
+// }
+
+
+//SO IN JAVA THERE THERE IS NOTHING LIKE FUNCTION WHICH IS STANDALONE THERE IS METHOD WHICH IS USED THE MAIN CLASS AND THERE IS TWO TYPE TO WRITE THE METHOD SO 1STLY USING STATIC DATAT TYPE IN WHICH WE DONT HAVE TO CREATE A OBJECT IN OUR MAIN METHOD AND  2CONDLY CREATING A METHOD WITH VOID DATA TYPE IN WHICH WE HAVE TO CRAETE A OBJECT BY WHICH WE CALL METHOD 
+
+// //EXAMPLE BY USING STATIC
+
+// import java.util.*;
+// import java.io.*;
+// public class Method{
+//     public static void printMyName(String Name){//SO HERE WE USE STATIC WITH THE DATA TYPE (VOID) BY WHICH WE DONT HAVE TO CREATE OBJECT IN OUR MAIN METHOD
+//         System.out.println(Name);
+//     }
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("ENTER YOUR NAME:");
+//         String your=sc.nextLine();
+//         printMyName(your);//HERE WE CALL OUR FUNCTION WITHOUT CREATING AN OBJECT
+//     }
+// }
+
+
+//NEXT EXAMPLE IS REMOVING STATIC AND THEN PRINT MY NAME BY CALLING MY FUNCTION BY CREATING OBJECT
+
+import java.util.*;
+import java.io.*;
+public class Method{
+    public void printMyName(String Name){
+          System.out.println(Name);
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        Method obj=new Method();
+        System.out.print("ENTER YOUR NAME:");
+        String your=sc.nextLine();
+        obj.printMyName(your);
+    }
+}
