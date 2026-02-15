3605681856ccae68c96c44f6f5bb5b307ed0d01a
a-nexus <anuragsingh77077@gmail.com>
2026-02-15 14:51:20 +0530

    QUESTION

--- Method.java
+++ Method.java
@@ -38,17 +38,59 @@
 
 //NEXT EXAMPLE IS REMOVING STATIC AND THEN PRINT MY NAME BY CALLING MY FUNCTION BY CREATING OBJECT
 
+// import java.util.*;
+// import java.io.*;
+// public class Method{
+//     public void printMyName(String Name){
+//           System.out.println(Name);
+//     }
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         Method obj=new Method();
+//         System.out.print("ENTER YOUR NAME:");
+//         String your=sc.nextLine();
+//         obj.printMyName(your);
+//     }
+// }
+
+
+//NEXT QUESTION MAKE A FUNCTION TO ADD 2 NUMBER AND RETURN THE SUM
+
+// import java.util.*;
+// public class Method{
+//     public static int calculateSum(int a,int b){
+//         int sum=a+b;
+//         return sum;
+//     }
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("ENTER THE FIRST NUMBER:");
+//         int a=sc.nextInt();
+//     System.out.print("ENTER THE SECOND NUMBER:");
+//         int b=sc.nextInt();
+        
+        
+//         int sum=calculateSum(a,b);
+//         System.out.println(sum);
+
+//     }
+// }
+
+//NEXT QUESTION MULTYPLYCATION OF TWO NUMBER BY FUNCTION OR METHOD
+
 import java.util.*;
-import java.io.*;
 public class Method{
-    public void printMyName(String Name){
-          System.out.println(Name);
+    public static int multiply(int a,int b){
+        int mul=a*b;
+        return mul;
     }
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        Method obj=new Method();
-        System.out.print("ENTER YOUR NAME:");
-        String your=sc.nextLine();
-        obj.printMyName(your);
+        System.out.print("Enter The First Value:");
+        int a=sc.nextInt();
+       System.out.print("Enter The Second Value:");
+        int b=sc.nextInt();
+        int mul=multiply(a,b);
+        System.out.println(mul);
     }
 }
