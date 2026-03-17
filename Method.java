85ecc3a7ef6024d6b515b8dbad919d55d95238d6
a-nexus <anuragsingh77077@gmail.com>
2026-03-17 22:56:17 +0530

    OUESTION

--- Method.java
+++ Method.java
@@ -317,72 +317,151 @@
 //USN,NAME,BRANCH,PHONE,PERCENTAGE.
 //WRITE A PROGRAM IN JAVA TO CREATE A STUDENT OBJECT AND PRINT THE USN,NAME,BRANCH,PHONE,PERCENTAGE OF THESE VARIABLE WITH SUITABLE HEADING 
 
-// import java.util.*;
-// public class Method{
-//     String usn;
-//     String name;
-//     String branch;
-//     String phone;
-//     double percentage;
-//     public static void main(String[] arg){
-//         Scanner sc=new Scanner(System.in);
-//         Method obj=new Method();
-//           System.out.print("ENTER THE UNIVERSITY SEAT NUMBER=");
-//           obj.usn=sc.nextLine();
-//           System.out.print("ENTER YOUR NAME=");
-//           obj.name=sc.nextLine();
-//            System.out.print("ENTER YOUR BRANCH NAME=");
-//           obj.branch=sc.nextLine();
-//             System.out.print("ENTER YOUR PHONE NUMBER=");
-//           obj.phone=sc.nextLine();
-//             System.out.print("ENTER YOUR MARCSHEET PERCENTAGE=");
-//           obj.percentage=sc.nextDouble(); 
-//           System.out.println("-----------STUDENT INFO-----------");
-//           System.out.println("USN="+obj.usn);
-//           System.out.println("NAME="+obj.name);
-//           System.out.println("BRANCH="+obj.branch);
-//           System.out.println("PHONE="+obj.phone);
-//           System.out.println("PERCENTAGE="+obj.percentage);
-//         }
-// }
-
+//    
 //WRITE A PROGRAM TO CRATE A CLASS CALLED SHAPE WITH METHOD CALLED GETPARAMETER () & GETAREA  CREATE A SUBCLASS CALLED CIRCLE THAT OVERRIDE THE GET PARAMETER AND GETAREA () METHOD TO CALCUTE AREA & PERIMETER OF CIRCLE 
 
-import java.util.*;
+// import java.util.*;
 
- class Shape{
-    public void getParameter(){
-        System.out.println("PARAMETER OF THE CIRCLE");
-    }
-    public void getArea(){
-        System.out.println("AREA OF THE CIRCLE");
-    }
+//  class Shape{
+//     public void getParameter(){
+//         System.out.println("PARAMETER OF THE CIRCLE");
+//     }
+//     public void getArea(){
+//         System.out.println("AREA OF THE CIRCLE");
+//     }
    
-    }
-     class Circle extends Shape{
-        double radius;
-        Circle(double r){
-            radius=r;
-        }
-        @Override
-       public void getParameter(){
-            double parameter=2* Math.PI * radius;
-           System.out.println("PARAMETER OF THE CIRCLE="+parameter);
-        }
-        @Override
-        public void getArea(){
-            double area=Math.PI*radius*radius;
-            System.out.println("AREA OF THE CIRCLE="+area);
-        }
- }
-public class Method{
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-     System.out.print("ENTER THE RADIUS:");
-     double r=sc.nextDouble();
-        Circle obj=new Circle(r);
-        obj.getParameter();
-        obj.getArea();
-    }
+//     }
+//      class Circle extends Shape{
+//         double radius;
+//         Circle(double r){
+//             radius=r;
+//         }
+//         @Override
+//        public void getParameter(){
+//             double parameter=2* Math.PI * radius;
+//            System.out.println("PARAMETER OF THE CIRCLE="+parameter);
+//         }
+//         @Override
+//         public void getArea(){
+//             double area=Math.PI*radius*radius;
+//             System.out.println("AREA OF THE CIRCLE="+area);
+//         }
+//  }
+// public class Method{
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//      System.out.print("ENTER THE RADIUS:");
+//      double r=sc.nextDouble();
+//         Circle obj=new Circle(r);
+//         obj.getParameter();
+//         obj.getArea();
+//     }
 
+// }
+// import java.util.*;
+// class Shape{
+//     void getParameter(){
+//         System.out.print("FIONDING PAREAMETER");
+//     }
+//     void getArea(){
+//         System.out.print("FINDING AREA");
+//     }
+
+// }
+// class Circle extends Shape{
+//     double radius;
+//     Circle(double r){
+//     radius=r;
+//     }
+//     @Override
+//     void getParameter(){
+//         double parameter=2*Math.PI*radius;
+//         System.out.println("THE PARAMETER OF THE CIRCLE="+parameter);
+//     }   
+//     @Override
+//     void getArea(){
+//         double area=Math.PI*radius*radius;
+//         System.out.println("THE AREA OF THE CIRCLE="+area);
+//     }
+// }
+// public class Method{
+//     public static void main(String[] args){
+//       Scanner sc=new Scanner(System.in);
+//       System.out.print("ENTER THE RADIUS OF THE CIRCLE:");
+//       double r=sc.nextDouble();
+//       Circle obj=new Circle(r);
+//       obj.getParameter();
+//       obj.getArea();
+//     }
+// }
+
+//WAJP TO CREAT A CLASS EMPLOYEE WITH THE METHOD CALLED CALCULATE SALARY CREATE TWO SUB CLASS MANAGER AND STAFF IN EACH SUB CLASS OVERRIDE THE CALCULATE SALARY TO CALCULATE AND RETURN THE SALARY BASED ON THEIR SPEICIFIC ROLE
+
+import java.util.*;
+class Employee{
+    void calculateSalary(){
+        System.out.println("CALCULATE SALARY");
+    }
+}
+class Manager extends Employee{
+    int a,b,c,d;
+    Manager(int grossSalary,int netSalary,int allowances,int perquisite){
+        a=grossSalary;
+        b=netSalary;
+        c=allowances;
+        d=perquisite;
+    }
+     @Override
+     void calculateSalary(){
+       int salary=a+b+c+d;
+       System.out.println("THE SALARY OF THE MANAGER="+salary);
+     }
+}
+class Staff extends Employee{
+    int a,b,c,d;
+    Staff(int grossSalary,int netSalary,int allowance,int perquisite){
+        a=grossSalary;
+        b=netSalary;
+        c=allowance;
+        d=perquisite;
+    }
+    @Override
+    void calculateSalary(){
+        int salary=a+b+c+d;
+        System.out.println("THE SALARY OF THE STAFF="+salary);
+    }
+}
+public class Method{
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("ENTER 'M' IF YOU ARE MANAGER AND INTER 'S' IF YOU ARE EMPLOYEE:");
+        String alpha=sc.next();
+        switch(alpha){
+            case "M" : System.out.print("ENTER THE GROSS SALARY:");
+                      int gs=sc.nextInt();
+                      System.out.print("ENTER THE NET SALARY:");
+                      int ns=sc.nextInt();
+                      System.out.print("ENTER THE ALLOWANCES:");
+                      int aw=sc.nextInt();
+                      System.out.print("ENTER THE PERQUISITE:");
+                      int pr=sc.nextInt();
+                      Manager obj=new Manager(gs,ns,aw,pr);
+                      obj.calculateSalary();
+                      break;
+            case "S":System.out.print("ENTER THE GROSS SALARY:");
+                      int gas=sc.nextInt();
+                      System.out.print("ENTER THE NET SALARY:");
+                      int nas=sc.nextInt();
+                      System.out.print("ENTER THE ALLOWANCES:");
+                      int aaw=sc.nextInt();
+                      System.out.print("ENTER THE PERQUISITE:");
+                      int par=sc.nextInt();
+                      Staff obj1=new Staff(gas,nas,aaw,par);
+                      obj1.calculateSalary();
+                      break;
+            default : System.out.println("INVALID INPUT");
+
+
+        }
+    }
 }
